6ac90515d82ef8c0b1f5c4fd976955d3870c0467
Shreyansh <dubeyshreyansh2003@gmail.com>
2026-03-15 13:46:10 +0530

    fix vm polyfill

--- config-overrides.js
+++ config-overrides.js
@@ -8,12 +8,14 @@ module.exports = function override(config) {
     buffer: require.resolve("buffer/"),
     stream: require.resolve("stream-browserify"),
     crypto: require.resolve("crypto-browserify"),
-    assert: require.resolve("assert/")
+    assert: require.resolve("assert/"),
+    vm: require.resolve("vm-browserify")
   };
 
   config.plugins.push(
     new webpack.ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
+      process: "process/browser"
     })
   );
 
